9482ce230611f82d40b6804117d370763d6d2a5c
vshleifman <vshleifman@gmail.com>
2021-01-10 15:47:52 +0000

    started working on express

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import './db/mongoose';
+
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,25 +1,14 @@
 import mongoose from 'mongoose';
-import PropertyModel from '../models/PropertyModel';
-import UserModel from '../models/UserModel';
 
 mongoose.connect('mongodb://mongo:27017/AtlasHouse-api', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', async () => {
-	// let user = UserModel.create({
-	// 	firstName: 'Victor',
-	// 	lastName: 'Shleifman',
-	// 	email: 'test@gmail.com',
-	// 	password: 'passtest',
-	// });
-	// let property = PropertyModel.create({
-	// 	name: 'TestRoom',
-	// 	codeID: '43C',
-	// 	price: '455',
-	// });
+	console.log('mongoose connected to db');
 });
