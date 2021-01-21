706e69bc8bc514ea1fcca1efa0a715461bff4214
vshleifman <vshleifman@gmail.com>
2021-01-21 18:18:12 +0000

    separated admin routes and services from user ones

--- src/api/UserRoutes.ts
+++ src/api/UserRoutes.ts
@@ -1,35 +1,17 @@
 import express from 'express';
-import auth from '../middleware/auth';
-import UserService from '../services/UserService';
-
+import auth, { Req } from '../middleware/auth';
+import UserServices from '../services/UserServices';
 const router = express.Router();
 
-router.get('/users', auth, async (req, res, next) => {
-	try {
-		const result = await UserService.getAll();
-		res.status(200).send(result);
-	} catch (error) {
-		next(error);
-	}
-});
-
-router.get('/users/:id', async (req, res, next) => {
-	const id = req.params.id;
-
-	try {
-		const result = await UserService.getOne(id);
-		res.status(200).send(result);
-	} catch (error) {
-		console.log(error);
-		next(error);
-	}
+router.get('/users/me', auth, async (req: Req, res) => {
+	res.send(req.user);
 });
 
 router.patch('/users/:id', async (req, res, next) => {
 	const id = req.params.id;
 
 	try {
-		const result = await UserService.update(id, req.body);
+		const result = await UserServices.update(id, req.body);
 		res.status(200).send(result);
 	} catch (error) {
 		next(error);
@@ -40,7 +22,7 @@ router.delete('/users/:id', auth, async (req, res, next) => {
 	const id = req.params.id;
 
 	try {
-		const result = await UserService.deleteOne(id);
+		const result = await UserServices.deleteOne(id);
 		res.status(200).send(result);
 	} catch (error) {
 		next(error);
