2191178682f01c3c78abbbbffd61e0c48ce45d91
vshleifman <vshleifman@gmail.com>
2021-02-15 16:48:46 +0000

    mostly fixed types in queryExtractor

--- src/api/extractQuery.ts
+++ src/api/extractQuery.ts
@@ -2,31 +2,30 @@ import { ReturnModelType } from '@typegoose/typegoose';
 import { Booking } from 'models/BookingModel';
 import { Property } from 'models/PropertyModel';
 import { ProtoUser } from 'models/UserModel';
-import { QueryOptions } from 'types/types';
+import { PartialSchemaClassIntersection, QueryOptions } from 'types/types';
+
+type Match = {
+	[key: string]: keyof PartialSchemaClassIntersection;
+};
 
 const extractQuery = (
 	model: ReturnModelType<typeof Booking | typeof ProtoUser | typeof Property>,
 	query: any,
 ): {
-	match: Partial<Booking | ProtoUser | Property>;
+	match: Match;
 	options: QueryOptions;
 } => {
 	const modelObject = new model();
-
-	const match: Partial<Booking | ProtoUser | Property> = {};
+	const match: Match = {};
 	const options: QueryOptions = { sort: {} };
 
 	Object.keys(query).forEach(key => {
 		if (key in modelObject) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
 			match[key] = query[key];
 		} else if (key === 'sortBy') {
 			const parts = query[key].split(':');
 			options.sort![parts[0]] = parts[1] === 'desc' ? -1 : 1;
 		} else {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
 			options[key] = parseInt(query[key]);
 		}
 	});
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,7 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { Request } from 'express';
+import { Booking } from '../models/BookingModel';
+import { Property } from '../models/PropertyModel';
 import { ProtoUser } from '../models/UserModel';
 
 export interface Req extends Request {
@@ -13,7 +15,12 @@ export enum UserType {
 }
 
 export interface QueryOptions {
+	[key: string]: number | undefined | { [key: string]: number };
 	limit?: number;
 	skip?: number;
 	sort?: { [key: string]: number };
 }
+
+export type PartialSchemaClassIntersection = Partial<
+	Booking & ProtoUser & Property
+>;
